01c2b843546fad763af4e3befad6f182f643dab4
Wei-Chun, Chang <wcchang@infuseai.io>
2023-09-05 17:21:24 +0800

    Rewrite dbt metric to dbt 1.6 metric
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- models/marts/commit_weekly.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-select *
-from {{ metrics.calculate(
-    [metric('total_commits'), metric('active_authors')],
-    dimensions=['repo'],
-    grain='week',
-    start_date='2022-07-01'
-)}}
